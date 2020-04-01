675a459c53bc343129a8a580110e308c35912f63
shono-koga <shono.koga@gmail.com>
2020-04-02 00:32:56 +0900

    setup urql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,20 @@
-export default () => (
-  <div className="hello">
-    <p>
-      Hello World! Here's a secret shared with the client using Next env:{' '}
-      <strong>{process.env.SECRET}</strong>, the secret is shared at build time,
-      which means every reference to the secret is replaced with its value
-    </p>
-    <style jsx>{`
-      .hello {
-        font: 15px Helvetica, Arial, sans-serif;
-        background: #eee;
-        padding: 100px;
-        text-align: center;
-        transition: 100ms ease-in background;
-      }
-      .hello:hover {
-        background: #ccc;
-      }
-    `}</style>
-  </div>
-)
+import React from "react";
+import { dedupExchange, fetchExchange } from "@urql/core";
+import { cacheExchange } from "@urql/exchange-graphcache";
+import { withUrqlClient, SSRExchange } from "next-urql";
+
+const mergeExchanges = (ssrExchange: SSRExchange) => [
+  dedupExchange,
+  cacheExchange({}),
+  ssrExchange,
+  fetchExchange
+];
+
+const Index = () => {
+  return <></>;
+};
+
+export default withUrqlClient(
+  { url: "https://graphql-pokemon.now.sh" },
+  mergeExchanges
+)(Index);
