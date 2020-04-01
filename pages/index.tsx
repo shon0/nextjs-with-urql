0db228abe91f4c1c68fcf357e9e654aaeb2acae0
shono-koga <shono.koga@gmail.com>
2020-04-02 01:11:14 +0900

    request query and display result

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { dedupExchange, fetchExchange } from "@urql/core";
 import { cacheExchange } from "@urql/exchange-graphcache";
 import { withUrqlClient, SSRExchange } from "next-urql";
+import { useQuery } from "urql";
+import gql from "graphql-tag";
+import "isomorphic-unfetch";
 
 const mergeExchanges = (ssrExchange: SSRExchange) => [
   dedupExchange,
@@ -10,8 +13,46 @@ const mergeExchanges = (ssrExchange: SSRExchange) => [
   fetchExchange
 ];
 
+const queryPokemon = gql`
+  query pokemon($first: Int!) {
+    pokemons(first: $first) {
+      id
+      name
+      types
+      image
+    }
+  }
+`;
+
+type Result = {
+  pokemons: {
+    id: string;
+    name: string;
+    types: string[];
+    image: string;
+  }[];
+};
+
 const Index = () => {
-  return <></>;
+  const [result] = useQuery<Result>({
+    query: queryPokemon,
+    variables: { first: 9 }
+  });
+
+  if (result.fetching) return <div>loading...</div>;
+  if (result.error) return <div>error...</div>;
+
+  return (
+    <div>
+      {result.data.pokemons.map(p => (
+        <div key={p.id}>
+          <h3>{p.name}</h3>
+          <div>{p.types.join(" / ")}</div>
+          <img src={p.image} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default withUrqlClient(
